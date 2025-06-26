56c5413b0934a6d4c3bf3108d0aacaeabd49a525
Tich Majena <tichmajena@gmail.com>
2025-06-26 15:17:41 +0200

    team card

--- src/lib/cards/TeamCard.svelte
+++ src/lib/cards/TeamCard.svelte
@@ -12,7 +12,7 @@
 		alt={member.name}
 	/>
 	<div
-		class="absolute bottom-0 h-1/2 w-full bg-linear-to-t from-black/90 via-black/70 to-transparent transition-all duration-250"
+		class="absolute bottom-0 h-1/2 w-full bg-linear-to-t from-black via-black/40 to-transparent transition-all duration-250"
 	></div>
 	<div class="absolute bottom-0 flex flex-col p-5">
 		<div class="w-full text-xl font-light">{member.name}</div>
