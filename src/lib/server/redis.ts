f86746bebb1173e9cd3a67e78647ad1b90a66d63
Tich Majena <tichmajena@gmail.com>
2025-07-15 19:22:35 +0200

    bot blocking2

--- src/lib/server/redis.ts
+++ src/lib/server/redis.ts
@@ -3,35 +3,35 @@ import { Ratelimit } from '@upstash/ratelimit';
 import { UPSTASH_REDIS_TOKEN, UPSTASH_REDIS_URL } from '$env/static/private';
 
 export const redis = new Redis({
-  url: UPSTASH_REDIS_URL,
-  token: UPSTASH_REDIS_TOKEN
+	url: UPSTASH_REDIS_URL,
+	token: UPSTASH_REDIS_TOKEN
 });
 
 export const ratelimit = {
-  create: new Ratelimit({
-    redis,
-    analytics: true,
-    prefix: 'ratelimit:create',
-    limiter: Ratelimit.slidingWindow(1, '15s')
-  }),
-  delete: new Ratelimit({
-    redis,
-    analytics: true,
-    prefix: 'ratelimit:delete',
-    limiter: Ratelimit.slidingWindow(1, '15s')
-  })
+	create: new Ratelimit({
+		redis,
+		analytics: true,
+		prefix: 'ratelimit:create',
+		limiter: Ratelimit.slidingWindow(1, '360s')
+	}),
+	delete: new Ratelimit({
+		redis,
+		analytics: true,
+		prefix: 'ratelimit:delete',
+		limiter: Ratelimit.slidingWindow(1, '360s')
+	})
 };
 export const daylimit = {
-  create: new Ratelimit({
-    redis,
-    analytics: true,
-    prefix: 'ratelimit:create',
-    limiter: Ratelimit.slidingWindow(1, '15s')
-  }),
-  delete: new Ratelimit({
-    redis,
-    analytics: true,
-    prefix: 'ratelimit:delete',
-    limiter: Ratelimit.slidingWindow(1, '15s')
-  })
+	create: new Ratelimit({
+		redis,
+		analytics: true,
+		prefix: 'ratelimit:create',
+		limiter: Ratelimit.slidingWindow(1, '15s')
+	}),
+	delete: new Ratelimit({
+		redis,
+		analytics: true,
+		prefix: 'ratelimit:delete',
+		limiter: Ratelimit.slidingWindow(1, '15s')
+	})
 };
